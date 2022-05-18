84ae50045c11a581ddf73cab3659b20e995d2595
Daniel Lessa <daniellsf@id.uff.br>
2022-05-18 03:33:16 -0300

    Finalizando grupo focal

--- pages/grupo-focal/index.jsx
+++ pages/grupo-focal/index.jsx
@@ -21,22 +21,31 @@ export default function GrupoFocal() {
         </h1>
 
         <p className={styles.description}>
-          Foi realizado um Grupo Focal para entender melhor a opnião sobre educação financeira no Brasil e a conexão do relacionamento entre pessoas e o dinheiro. Todos concordaram com os mesmos termos dos formulários.
+          Foi realizado um Grupo Focal discutindo sobre as perguntas no formulário e assim entender melhor a opnião sobre educação financeira no Brasil e a conexão do relacionamento entre pessoas e o dinheiro. Todos concordaram com os mesmos termos dos formulários.
         </p>
 
         <div className={styles.info}>
           <p>A reunião foi feita com 3 entrevistados</p>
           <ul>
-            <li>Desempregado, sem conhecimentos sobre investimentos e que pega dinheiro emprestado</li>
-            <li>Empregado a pouco tempo, não faz investimos e empresta dinheiro</li>
-            <li>Empregado a muito tempo, faz investimentos e não tem hábito de emprestar dinheiro</li>
+            <li>Desempregado, sem conhecimentos sobre investimentos e que pega dinheiro emprestado;</li>
+            <li>Empregado a pouco tempo, não faz investimos e empresta dinheiro;</li>
+            <li>Empregado a muito tempo, faz investimentos e não tem hábito de emprestar dinheiro;</li>
           </ul>
 
-          <p>Conversamos sobre as perguntas do questionário e ao final da reunião chegamos a alguns consensos</p>
+          <p>Consenso geral</p>
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>As primeiras informações deveriam ser quem deve você e a quem você deve;</li>
+            <li>Seria interessante ter dicas de como gerenciar melhor seu dinheiro;</li>
+            <li>É muito importante ter datas nos empréstimos;</li>
+            <li>Ter um lugar de fácil acesso aos gastos atuais e um histórico é interessante;</li>
+            <li>Todos já passaram pela situação de dever ou estarem te devendo;</li>
+          </ul>
+
+          <p>Pontos importantes</p>
+          <ul>
+            <li>Lembretes na semana de vencimento da conta são importantes;</li>
+            <li>O que imprestou dinheiro ficava sem saber como cobrar;</li>
+            <li>Esses dados são bem sensíveis e devem ser protegidos;</li>
           </ul>
         </div>
       </main>
