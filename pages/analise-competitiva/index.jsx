1dbb226c2538d3fb1059ae7b6a65e67e68cb28ff
Daniel Lessa <daniellsf@id.uff.br>
2022-05-18 00:45:24 -0300

    Finalizando analise competitiva

--- pages/analise-competitiva/index.jsx
+++ pages/analise-competitiva/index.jsx
@@ -21,63 +21,71 @@ export default function AnaliseCompetitiva() {
       logo: Splitwise,
       description: 'Facilita a divisão de contas com amigos e familiares. Organiza todas as suas despesas compartilhadas em um só lugar, para que todos possam ver a quem devem.',
       positives: [
-        'Armazenamento de dados na nuvem podendo ser acessado em qualquer lugar: no iPhone, Android ou no computador.',
-        'Integração entre vários usuários',
-        'Navegação offline',
-        'Integração com pagamentos',
-        'Calculo de saldos totais',
-        'Simplificação das dívidas',
-        'Categorização das despesas'
+        'Armazenamento de dados na nuvem podendo ser acessado em qualquer lugar: no iPhone, Android ou no computador;',
+        'Integração entre vários usuários;',
+        'Navegação offline;',
+        'Integração com pagamentos de outras plataformas;',
+        'Cálculo de saldos totais;',
+        'Simplificação das dívidas;',
+        'Categorização das despesas;'
       ],
       negatives: [
-        'Muitos dados na mesma tela'
+        'Totalmente em inglês;',
+        'Funcionalidades excluisvas na versão paga;'
       ],
-      conclusion: ''
+      conclusion: 'A organização em categorias é uma ótima ideia para organizar de uma forma melhor o conteúdo. Além disso, a forma que é feita interação de vários usuários pode servir de inspiração, por exemplo da forma que é calculada a dívida entre usuários.'
     },
     {
       id: 3, 
       link: 'https://picpay.com/site',
       name: 'PicPay', 
       logo: PicPay,
-      description: 'Funciona como uma carteira digital. O programa permite fazer compras pelo smartphone com cartão de crédito ou valor de transferência',
+      description: 'Funciona como uma carteira digital. O programa permite fazer compras pelo smartphone com cartão de crédito ou valor de transferência.',
       positives: [
-        'Opção de compartilhar os gastos'
+        'Opção de compartilhar os gastos e cobrar depois, bem parecido com o acompanhamento que será feito;',
+        'Conhecido pela sua segurança;',
+        'Fácil utilização;',
+        'Tela de início mostrando as principais funcionalidades de forma bem acessível;'
       ],
-      negatives: [''],
-      conclusion: ''
+      negatives: [
+        'Muitas funcionalidades podendo gerar uma confusão em um primeiro momento;',
+        'Em alguns fluxos aparecem muitos dados nas telas, como por exemplo na loja;'
+      ],
+      conclusion: 'Podemos olhar bastante em como funciona o fluxo de dividir um gasto, pois parece ser bem eficiente e seguro.'
     },
     {
       id: 4, 
       link: 'https://www.organizze.com.br/',
       name: 'Organizze', 
       logo: Organizze,
-      description: 'Organiza o dinheiro em tempo real oferecendo práticidade e segurança, com a garantia de controle total das suas finanças.',
+      description: 'Organiza o dinheiro em tempo real oferecendo praticidade e segurança, com a garantia de controle total das suas finanças.',
       positives: [
-        'Alertas: Receba alertas de todas as suas contas a pagar e dê adeus aos juros',
-        'Relatórios: Resumos incríveis, com gráficos simples e completos',
-        'Categorização dos gastos',
-        'Bem organizado'
+        'Alertas: receba alertas de todas as suas contas a pagar e dê adeus aos juros;',
+        'Relatórios: resumos incríveis, com gráficos simples e completos;',
+        'Categorização dos gastos;',
+        'Os menus são muito bem organizado;'
       ],
       negatives: [
-        'Muita informação e opções',
-        'É pago'
+        'É pago;'
       ],
-      conclusion: ''
+      conclusion: 'Aplicativo muito bom para organizar despesas. Em questão de eficiência e utilidade é uma ótima referencia, principalmente observando os relatórios gerados.'
     },
     {
       id: 5, 
       link: 'https://www.meudinheiroweb.com.br/',
       name: 'Meu Dinheiro', 
       logo: MeuDinheiro,
-      description: '',
+      description: 'Sistema para controle financeiro online.',
       positives: [
         'Controle de Metas',
-        'Centro de Custos e Lucros'
+        'Centro de Custos e Lucros',
+        'Bem organizado'
       ],
       negatives: [
-        'É pago'
+        'É pago',
+        'Poucas funcionalidades'
       ],
-      conclusion: ''
+      conclusion: 'Visando a parte de eficácia, os resumos de custos e lucros são ótimas referências.'
     }
   ];
 
@@ -123,7 +131,7 @@ export default function AnaliseCompetitiva() {
         </h1> 
 
         <p className={styles.description}>
-          Concorrentes que possuem modelos de negócio e idéias inspiradores para o projeto
+          Concorrentes que possuem modelos de negócio e ideias inspiradores para o projeto
         </p>
 
         <table className='table table-bordered'>
