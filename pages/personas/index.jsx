781b4ab4e9e739ee1cd4077c528dc8cf102e9eaf
Daniel Lessa <daniellsf@id.uff.br>
2022-06-25 01:31:27 -0300

    Ajustando personas

--- pages/personas/index.jsx
+++ pages/personas/index.jsx
@@ -31,14 +31,15 @@ export default function Questionario() {
           <p>Cenário:</p>
           <p>A gata de Fábio ficou doente e ele precisa levá-la no veterinário, porém é caro e já é final do mês. Como de costume, Fábio tem idéia de entrar em contato com Diego para pedir dinheiro emprestado, mas seu amigo ficam na dúvida de ajudá-lo, pois não é a primeira vez que Fábio pede dinheiro emprestado, e da última vez foi para comprar um par de sapatos, no qual ele esqueceu por muito tempo de pagar e Diego ficou sem graça de lembrar.</p>
           <p>Tarefas:</p>
+          <p>Tarefas:</p>
           <ol>
             <li>
               Objetivo: pedir dinheiro emprestado
               <ol>
                 <li>
-                  Método: pedir dinheiro emprestado
+                  Método: encontrar amigo pelo fluxo de comunidade
                   <ol>
-                    <li>Operador: pedir dinheiro emprestado</li>
+                    <li>Operador: clica na opção de adicionar novo amigo</li>
                     <li>Operador: busca Diego na lista de usuários cadastrados</li>
                     <li>Operador: clica na opção de Pedir</li>
                     <li>Operador: preenche o formulário com dados para o empréstimo</li>
@@ -76,44 +77,31 @@ export default function Questionario() {
         <Image src={PersonaFerdinando} alt={`Persona do Ferdinando`} />
         <div className={styles.info}>
           <p>Cenário:</p>
-          <p></p>
-          <p>Tarefas:</p>
+          <p>A Black Friday ainda será em alguns meses, mas Ferdinando já está fazendo planejamentos para ver se é possível melhorar seu computador com uma nova placa de vídeo. Aprendendo com os ensinamentos do passado, ele está tentando gerenciar seu dinheiro já contando com uma margem caso tenha algum imprevisto. Além disso, ele sabe que no período de tempo até o dia da promoção, ele pode não ter o dinheiro suficiente, então está combinando com seu pai a possibilidade de pegar emprestado e devolver na frente com um valor maior, mesmo que o pai não goste muito da ideia pois é seu filho.</p>
           <ol>
             <li>
-              Objetivo: pedir dinheiro emprestado
+              Objetivo: verificar como foram seus gastos e empréstimos nos últimos meses
               <ol>
                 <li>
-                  Método: pedir dinheiro emprestado
+                  Método: verificar seus relatórios e histórico no menu de relatórios
                   <ol>
-                    <li>Operador: pedir dinheiro emprestado</li>
-                    <li>Operador: busca Diego na lista de usuários cadastrados</li>
-                    <li>Operador: clica na opção de Pedir</li>
-                    <li>Operador: preenche o formulário com dados para o empréstimo</li>
-                    <li>Operador: confirma pedido de empréstimo</li>
+                    <li>Operador: clica em relatórios</li>
+                    <li>Operador: clica na opção de gestão geral</li>
                   </ol>
                 </li>
               </ol>
             </li>
             <li>
-              Objetivo: lembrar Fábio do dinheiro emprestado
+              Objetivo: registrar sua dívida com um maior retorno
               <ol>
                 <li>
-                  Método: encontrar amigo pelo fluxo de comunidade
+                  Método: encontrar o pai pelo menu de comunidade
                   <ol>
-                    <li>Operador: clica na opção de comunidade</li>
-                    <li>Operador: escreve o nome de Fábio na lista de resultados</li>
-                    <li>Operador: clica em Fábio</li>
-                    <li>Operador: clica no empréstimo que deseja ver dentro da lista de empréstimos na visualização do Fábio</li>
-                    <li>Operador: clica em Lembrar</li>
-                  </ol>
-                </li>
-                <li>
-                  Método: encontrar empréstimo pelos relatórios
-                  <ol>
-                    <li>Operador: clica em relatórios na barra de navegação</li>
-                    <li>Operador: clica em empréstimos feitos</li>
-                    <li>Operador: clica no empréstimo não pago que deseja cobrar</li>
-                    <li>Operador: clica em Lembrar</li>
+                    <li>Operador: busca pelo pai na lista de amigos adicionados</li>
+                    <li>Operador: clica na pai</li>
+                    <li>Operador: clica no botão de Pedir</li>
+                    <li>Operador: preenche o formulário com dados para o empréstimo prestando atenção no valor de retorno</li>
+                    <li>Operador: confirma pedido de empréstimo</li>
                   </ol>
                 </li>
               </ol>
