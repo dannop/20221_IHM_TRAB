1e03bedc329dc3eae8815e14a937bf46b224ef82
Daniel Lessa <daniellsf@id.uff.br>
2022-07-16 00:15:24 -0300

    Ajustando link na navegacao

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Image from 'next/image'
 
 import PayMe from '../../assets/images/PayMeTransparent.png'
@@ -5,10 +6,12 @@ import PayMe from '../../assets/images/PayMeTransparent.png'
 export default function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a className="navbar-brand" href="/">
-          <Image src={PayMe} alt="Pay.me Logo" width={50} height={50}/>
-        </a>
+      <div className="container-fluid">
+        <Link href="/">
+          <a className="navbar-brand">
+            <Image src={PayMe} alt="Pay.me Logo" width={50} height={50}/>
+          </a>
+        </Link>
 
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -17,20 +20,32 @@ export default function Header() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="/">Início</a>
+              <Link href="/">
+                <a className="nav-link">Início</a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/informacoes">Informações</a>
+              <Link href="/informacoes">
+                <a className="nav-link">Informações</a>
+              </Link>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" id="navbarAnaliseSituacao" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Análise da Situação 
               </a>
               <div className="dropdown-menu" aria-labelledby="navbarAnaliseSituacao">
-                <a className="dropdown-item" href="/desk-research">Desk Research</a>
-                <a className="dropdown-item" href="/analise-competitiva">Análise Competitiva</a>
-                <a className="dropdown-item" href="/questionario">Questionário</a>
-                <a className="dropdown-item" href="/grupo-focal">Grupo Focal</a>
+                <Link href="/desk-research">
+                  <a className="dropdown-item">Desk Research</a>
+                </Link>
+                <Link href="/nalise-competitiva">
+                  <a className="dropdown-item">Análise Competitiva</a>
+                </Link>
+                <Link href="/questionario">
+                  <a className="dropdown-item">Questionário</a>
+                </Link>
+                <Link href="/grupo-focal">
+                  <a className="dropdown-item">Grupo Focal</a>
+                </Link>
               </div>
             </li>
             <li className="nav-item dropdown">
@@ -38,8 +53,12 @@ export default function Header() {
                 Síntese e Prototipação
               </a>
               <div className="dropdown-menu" aria-labelledby="navbarSinteseProtipacao">
-                <a className="dropdown-item" href="/personas">Personas e Cenários</a>
-                <a className="dropdown-item" href="/prototipo">Protótipo</a>
+                <Link href="/personas">
+                  <a className="dropdown-item">Personas e Cenários</a>
+                </Link>
+                <Link href="/prototipo">
+                  <a className="dropdown-item">Protótipo</a>
+                </Link>
               </div>
             </li>
             <li className="nav-item dropdown">
@@ -47,9 +66,15 @@ export default function Header() {
                 Avaliação da Interação
               </a>
               <div className="dropdown-menu" aria-labelledby="navbarAvaliacaoInteracao">
-                <a className="dropdown-item" href="/avaliacao-interacao/first-click">First Click</a>
-                <a className="dropdown-item" href="/avaliacao-interacao/avaliacao-heuristica">Avaliação Heurística</a>
-                <a className="dropdown-item" href="/avaliacao-interacao/teste-usabilidade">Teste de Usabilidade</a>
+                <Link href="/avaliacao-interacao/first-click">
+                  <a className="dropdown-item">First Click</a>
+                </Link>
+                <Link href="/avaliacao-interacao/avaliacao-heuristica">
+                  <a className="dropdown-item">Avaliação Heurística</a>
+                </Link>
+                <Link href="/avaliacao-interacao/teste-usabilidade">
+                  <a className="dropdown-item">Teste de Usabilidade</a>
+                </Link>
               </div>
             </li>
           </ul>
